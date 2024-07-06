df4e9f38b64720a8e8b2d450e18e5f37e642b9d9
sara hoxhaj <12242040@univie.ac.at>
2024-07-06 18:14:00 +0200

    friend test

--- src/components/FriendTest.vue
+++ src/components/FriendTest.vue
@@ -19,16 +19,18 @@
             <h5 class="my-0 mr-md-auto" style="margin-left:37rem; position:absolute;">Friend test</h5>
         </div>
     </nav>
-
+    <div style="position: absolute; margin-top: 2rem; width: 100%; text-align: center;">
+        <p id="text-paragraph">Click on the buttons to see the result of each test</p>
+    </div>
     <div class="container justify-content-center"
-        style="margin-top: 6rem; border: 1px solid #DDD; width: 50rem; height: 30rem; overflow: hidden;">
+        style="margin-top: 6rem; border: 1px solid #DDD; width: 50rem; height: 30rem; overflow: hidden; position: relative;">
         <div id="bar-chart"></div>
-        <button @click.prevent="firstTest" style="margin-left:30rem; position:absolute;" type="button"
-            class="btn btn-outline-secondary">First test</button>
-        <button @click.prevent="secondTest" style="margin-left:30rem; position:absolute; margin-top: 3rem;" type="button"
-            class="btn btn-outline-secondary">Second test</button>
-        <button @click.prevent="thirdTest" style="margin-left:30rem; position:absolute; margin-top: 6rem;" type="button"
-            class="btn btn-outline-secondary">Third test</button>
+        <button @click.prevent="firstTest" style="position: absolute; right: 1rem; top: 1rem;" type="button"
+            class="btn btn-outline-secondary btn-sm">1<sup>st</sup> test</button>
+        <button @click.prevent="secondTest" style="position: absolute; right: 1rem; top: 3.5rem;" type="button"
+            class="btn btn-outline-secondary btn-sm">2<sup>nd</sup> test</button>
+        <button @click.prevent="thirdTest" style="position: absolute; right: 1rem; top: 6rem;" type="button"
+            class="btn btn-outline-secondary btn-sm">3<sup>rd</sup> test</button>
     </div>
 </template>
 
@@ -40,6 +42,10 @@ export default {
     data() {
         return {
             averageComplexities: [],
+            originalRank: [5, 26, 22, 8, 20, 3, 17, 14, 1, 4, 18, 19, 2, 6, 9, 11, 13, 21, 15, 25, 24, 7, 12, 10, 16, 13],
+            firstTestRank: [22, 6, 20, 6, 5, 18, 14, 24, 17, 11, 7, 12, 8, 9, 16, 1, 21, 23, 13, 15, 3, 4, 19, 2, 1, 25],
+            secondTestRank: [6, 22, 20, 19, 26, 14, 5, 8, 2, 21, 13, 3, 24, 10, 17, 12, 11, 4, 9, 1, 7, 15, 16, 23, 18, 25],
+            thirdTestRank: [22, 20, 6, 8, 5, 18, 21, 19, 26, 13, 16, 23, 2, 1, 3, 12, 4, 17, 15, 9, 14, 7, 11, 24, 25, 10],
         };
     },
     mounted() {
@@ -139,6 +145,12 @@ export default {
             const imageNames = ['image22', 'image26', 'image20', 'image6', 'image5', 'image18', 'image14', 'image24', 'image17', 'image11', 'image7', 'image12', 'image8', 'image9', 'image16', 'image1', 'image21', 'image23', 'image13', 'image15', 'image3', 'image4', 'image19', 'image2', 'image1', 'image25'];
             this.averageComplexities.sort((a, b) => imageNames.indexOf(a.key) - imageNames.indexOf(b.key));
 
+            const originalRank = this.originalRank.slice();
+            const firstTestRank = this.firstTestRank.slice();
+            const correlation = this.kendallsTau(originalRank, firstTestRank);
+            console.log('first Kendall\'s tau:', correlation);
+            document.getElementById('text-paragraph').innerText = 'Total time of 12 min & Kendall`s Tau = ' + correlation.toFixed(3);
+
             const svg = d3.select('#bar-chart').select('svg').select('g'); // Select the existing SVG group
 
             const svgWidth = 680;
@@ -175,8 +187,14 @@ export default {
                 .attr('height', d => height - y(d.avgComplexity));
         },
         secondTest() {
-            const imageNames = ['image6','image22','image20','image19','image26','image14','image5','image8','image2','image21','image13','image3','image24','image10','image17','image12','image11','image4','image9','image1','image7','image15','image16','image23','image18','image25',];
+            const imageNames = ['image6', 'image22', 'image20', 'image19', 'image26', 'image14', 'image5', 'image8', 'image2', 'image21', 'image13', 'image3', 'image24', 'image10', 'image17', 'image12', 'image11', 'image4', 'image9', 'image1', 'image7', 'image15', 'image16', 'image23', 'image18', 'image25',];
             this.averageComplexities.sort((a, b) => imageNames.indexOf(a.key) - imageNames.indexOf(b.key));
+
+            const originalRank = this.originalRank.slice();
+            const secondTestRank = this.secondTestRank.slice();
+            const correlation = this.kendallsTau(originalRank, secondTestRank);
+            console.log('second Kendall\'s tau:', correlation);
+            document.getElementById('text-paragraph').innerText = 'Total time of 6 min & Kendall`s Tau = ' + correlation.toFixed(3);
 
             const svg = d3.select('#bar-chart').select('svg').select('g'); // Select the existing SVG group
 
@@ -214,8 +232,14 @@ export default {
                 .attr('height', d => height - y(d.avgComplexity));
         },
         thirdTest() {
-            const imageNames = ['image22','image20','image6','image8','image5','image18','image21','image19','image26','image13','image16','image23','image2','image1','image3','image12','image4','image17','image15','image9','image14','image7','image11','image24','image25','image10'];
+            const imageNames = ['image22', 'image20', 'image6', 'image8', 'image5', 'image18', 'image21', 'image19', 'image26', 'image13', 'image16', 'image23', 'image2', 'image1', 'image3', 'image12', 'image4', 'image17', 'image15', 'image9', 'image14', 'image7', 'image11', 'image24', 'image25', 'image10'];
             this.averageComplexities.sort((a, b) => imageNames.indexOf(a.key) - imageNames.indexOf(b.key));
+
+            const originalRank = this.originalRank.slice();
+            const thirdTestRank = this.thirdTestRank.slice();
+            const correlation = this.kendallsTau(originalRank, thirdTestRank);
+            console.log('third Kendall\'s tau:', correlation);
+            document.getElementById('text-paragraph').innerText = 'Total time of 7 min & Kendall`s Tau = ' + correlation.toFixed(3);
 
             const svg = d3.select('#bar-chart').select('svg').select('g'); // Select the existing SVG group
 
@@ -252,6 +276,34 @@ export default {
                 .attr('y', d => y(d.avgComplexity))
                 .attr('height', d => height - y(d.avgComplexity));
         },
+        kendallsTau(originalRank, compareRank) {
+            if (originalRank.length !== compareRank.length) {
+                throw new Error('Arrays must have the same length');
+            }
+
+            let concordant = 0;
+            let discordant = 0;
+
+            for (let i = 0; i < originalRank.length; i++) {
+                for (let j = i + 1; j < originalRank.length; j++) {
+                    const oi = originalRank[i];
+                    const oj = originalRank[j];
+                    const ii = compareRank.indexOf(oi);
+                    const ij = compareRank.indexOf(oj);
+
+                    // Check if pairs are in the same order in both rankings
+                    if ((ii < ij && i < j) || (ii > ij && i > j)) {
+                        concordant++;
+                    } else {
+                        discordant++;
+                    }
+                }
+            }
+            const n = originalRank.length;
+            const kendallsTau = (concordant - discordant) / (0.5 * n * (n - 1));
+            return kendallsTau;
+        },
+
     }
 };
 </script>
